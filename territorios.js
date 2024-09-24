6b057ae9ec0e75d8904bbd09bfbd712ddc6ebc34
carlosAlexandre <142914111+carlosAlexandre7@users.noreply.github.com>
2024-09-24 08:05:33 -0300

    Update territorios.js

--- territorios.js
+++ territorios.js
@@ -1,72 +1,158 @@
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Pressable, TextInput } from 'react-native-web';
+import { StyleSheet, Text, Pressable, View } from 'react-native';
+import { Modal, ScrollView } from 'react-native-web';
+import { Button,Image } from 'react-native-web';
+import { useState } from 'react';
 
 export default function App() {
+  
+const [animal,setAnimal] = useState([
+  {
+
+  nome:'Nome: Animal1',
+  descrição:'Animal qualquer',
+  habitos:'Hábitos: ',
+  reproducao:'Reprodução: ',
+  conservacao:'Conservação: '
+  },
+  {
+
+  nome:'Nome: Animal2',
+  descrição:'Animal qualquer',
+  habitos:'Hábitos: ',
+  reproducao:'Reprodução: ',
+  conservacao:'Conservação: '
+  },
+  {
+
+  nome:'Nome: Animal3',
+  descrição:'Animal qualquer',
+  habitos:'Hábitos: ',
+  reproducao:'Reprodução: ',
+  conservacao:'Conservação: '
+  },
+  {
+  nome:'Nome: Animal4',
+  descrição:'Animal qualquer'
+  },
+  {
+  nome:'Nome: Animal5',
+  descrição:'Animal qualquer'
+  ,
+habitos:'Hábitos: ',
+reproducao:'Reprodução: ',
+conservacao:'Conservação: '},
+  {
+
+  nome:'Nome: Animal6',
+  descrição:'Animal qualquer',
+  habitos:'Hábitos: ',
+  reproducao:'Reprodução: ',
+  conservacao:'Conservação: '
+  }
+  ])
+
+  const[visivel, setVisivel] = useState(false);
+  const[indiceSelecionado, setIndiceSelecionado] = useState(0)
+  const carregar = (indice) => {
+    setIndiceSelecionado(indice);
+    setVisivel(true)
+  };
+  const voltar = () =>{
+    setVisivel(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <View>
-        <View style={styles.territorioArea}>
-          <Pressable style={styles.territorio}>
-            <Text>1</Text>
-          </Pressable>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.territorioArea}>
-          <Pressable style={styles.territorio}>
-            <Text>2</Text>
-          </Pressable>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.territorioArea}>
-          <Pressable style={styles.territorio}>
-            <Text>3</Text>
-          </Pressable>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.territorioArea}>
-          <Pressable style={styles.territorio}>
-            <Text>4</Text>
-          </Pressable>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.territorioArea}>
-          <Pressable style={styles.territorio}>
-            <Text>5</Text>
-          </Pressable>
-        </View>
-      </View>
-
-      <View>
-        <View style={styles.territorioArea}>
-          <Pressable style={styles.territorio}>
-            <Text>6</Text>
-          </Pressable>
-        </View>
-      </View>
-
+    <View style={(styles.container)} >
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.header}>
+        <Text>Cabeçalho</Text>
+      </View>
+      <View style={styles.botaoArea}>
+        <Pressable style={styles.botao} onPress={() => carregar (0)}>
+          <Text style={styles.nome}>Animal 1</Text>
+        </Pressable>
+      </View>
+      <View style={styles.botaoArea}>
+      <Pressable style={styles.botao} onPress={() => carregar (1)}>
+          <Text style={styles.nome}>Animal 2</Text>
+        </Pressable>
+      </View>
+      <View style={styles.botaoArea}>
+      <Pressable style={styles.botao} onPress={() => carregar (2)}>
+          <Text style={styles.nome}>Animal 3</Text>
+        </Pressable>
+      </View>
+      <View style={styles.botaoArea}>
+      <Pressable style={styles.botao} onPress={() => carregar (3)}>
+          <Text style={styles.nome}>Animal 4</Text>
+        </Pressable>
+      </View>
+      <View style={styles.botaoArea}>
+      <Pressable style={styles.botao} onPress={() => carregar (4)}>
+          <Text style={styles.nome}>Animal 5</Text>
+        </Pressable>
+      </View>
+      <View style={styles.botaoArea}>
+      <Pressable style={styles.botao} onPress={() => carregar (5)}>
+          <Text style={styles.nome}>Animal 6</Text>
+        </Pressable>
+      </View>
+      
+      <Modal visible={visivel} animationType="fade">
+        <View style={styles.modal}>
+          <Text style={styles.animal}>{animal[indiceSelecionado].nome}</Text>
+          <Text style={styles.animal}>{animal[indiceSelecionado].descrição}</Text>
+          <Text style={styles.animal}>{animal[indiceSelecionado].habitos}</Text>
+          <Text style={styles.animal}>{animal[indiceSelecionado].reproducao}</Text>
+          <Text style={styles.animal}>{animal[indiceSelecionado].conservacao}</Text>
+          <Button title="Voltar" onPress={() => voltar()}/>
+          <Image source={require} style={{width:50, height:50}}/>
+        </View>
+      </Modal>
+</View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection:'row'
+    backgroundColor: '#828282',
+    justifyContent:'center',
+    alignItens:'center'
   },
-  territorioArea:{
+  botaoArea:{
     flex:1,
+    width:'100%',
+    height:'100%',
+    alignItems:'center',
+    justifyContent:'center',
+    
+  },
+  botao:{
+    width:'95%',
+    height:'85%',
+    borderRadius:25,
+    justifyContent:'center',
+    backgroundColor:'#545454'
+  },
+  header:{
+    flex:0.5,
+  },
+  nome:{
+    textAlign:'center'
+  },
+  modal:{
+    margin:'5%',
+    alignSelf:'center',
+    justifyContent:'center',
+    width:'95%',
+    height:'95%',
+    backgroundColor:'#828282'
+  },
+  animal:{
+    height:'100%'
   }
+
 });
